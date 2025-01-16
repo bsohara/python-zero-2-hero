db4daebe465a857cb20089608bd2054c098b66d4
bsohara <b.sohara.git@gmail.com>
2025-01-16 18:19:49 -0300

    inserindo metodos de strings

--- secao1/7 - metodos de string.py
+++ secao1/7 - metodos de string.py
@@ -2,3 +2,22 @@
 mensagem = "Eu adoro churrasco, lanche e pizza"
 
 
+# em maiúsculo e minúsculo
+maiusculo = mensagem.upper()
+minusculo = mensagem.lower()
+
+print("em maiúsculo:",maiusculo)
+print("em minúsculo:",minusculo)
+
+# substutui caracteres ou palavras na frase
+mensagem_alterada = mensagem.replace("pizza", "comida japonesa")
+print(mensagem_alterada)
+
+mensagem_alterada2 = mensagem.replace("a", "i")
+print(mensagem_alterada2)
+
+# encontra o início da letra ou da palavra
+encontrar_churrasco = mensagem.index("churrasco")
+
+print("Onde começa?",encontrar_churrasco)
+
