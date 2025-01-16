ac07a4fb7e7654037cf5c56f13ea369961300c5a
bsohara <b.sohara.git@gmail.com>
2025-01-16 12:18:37 -0300

    introdução + strings e numeros

---
+++ secao1/introducao.py
@@ -0,0 +1,20 @@
+# Print e adição de comentários
+#a. print
+# print("Olá")
+# print("Olá Python")
+
+#b. adição de comentários
+# > não pode ser executado
+
+# não pode ser executado
+
+# > no exemplo acima, foi verificado o erro de "invalid syntax" (antes de colocar a hash de comentário)
+
+'''
+
+comentário de múltiplas linhas
+autor: fulano de tal
+data: 01/01/01
+versão: 1.0
+
+'''
