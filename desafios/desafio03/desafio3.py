e1fd65dd1ebad7ea3f99432e59bd2aacf2cf7213
bsohara <b.sohara.git@gmail.com>
2025-01-16 15:23:41 -0300

    resolução do desafio 03

---
+++ desafios/desafio03/desafio3.py
@@ -0,0 +1,9 @@
+num1 = int(input("Qual o primeiro número? "))
+num2 = float(input("Qual o segundo número? "))
+
+def divNum(a, b):
+    return a / b
+
+result = divNum(num1, num2)
+
+print(f"Resultado da divisão = {result}")
