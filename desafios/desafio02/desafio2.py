70a7e7f5f06534aac6a12b7305cd73fa88e6e647
bsohara <b.sohara.git@gmail.com>
2025-01-16 15:16:58 -0300

    resolução do código do desafio 02

---
+++ desafios/desafio02/desafio2.py
@@ -0,0 +1,10 @@
+# Atribuindo as variáveis
+name = input("Qual seu nome? ")         # string
+age = int(input("Qual sua idade? "))    # integer
+
+# Atribuindo uma variável que imprima as variáveis em um template string
+story = f'Olá! Meu nome é {name} e tenho {age} anos.' # string
+
+# Imprime
+print(story)
+
