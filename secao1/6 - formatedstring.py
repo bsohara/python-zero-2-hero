f83d647db9fcbe29f21ae97c48e809aa1df674e4
bsohara <b.sohara.git@gmail.com>
2025-01-16 17:21:30 -0300

    inserindo exemplos com e sem formated strings

---
+++ secao1/6 - formatedstring.py
@@ -0,0 +1,9 @@
+nome = 'Bruno'
+sobrenome = 'Sohara'
+profissao = 'Programador Frontend e Analista de Produtos de Rede'
+
+texto_sem_formated_strings = 'O ' + nome + ' ' + sobrenome + ' é [' + profissao + ']'
+texto_com_formated_strings = f'O {nome} {sobrenome} é [{profissao}]'
+
+print(texto_sem_formated_strings)
+print(texto_com_formated_strings)
