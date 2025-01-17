69915fae147cb962ef8092e06491371bcbad741e
bsohara <b.sohara.git@gmail.com>
2025-01-17 09:54:36 -0300

    operações matemáticas em python

---
+++ secao2/operacoes_matematicas.py
@@ -0,0 +1,33 @@
+'''
+--> ordenação de calculos
+
+1º = ** e sqrt()
+2º - * e /
+3º + e -
+'''
+
+'''
+exemplo de uma equação
+
+a = 9
+b = 8
+c = 7
+
+equacao = a * b + c # resultado: 79
+
+print(equacao)
+
+d = 6
+
+equacao2 = d ** 2 + equacao
+
+print(equacao2)
+'''
+a = 9
+b = 8
+c = 7
+d = 6
+
+equacao = a * b + (c + d) # resultado: 85 ==> aqui, o parênteses vem primeiro
+
+print(equacao)
