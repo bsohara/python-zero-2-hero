5946dd195fba415f291654972f1eb6700a0955d3
bsohara <b.sohara.git@gmail.com>
2025-01-17 10:52:01 -0300

    if_else => exemplo do dia-a-dia |==| operadores_logicos

--- secao3/if_else.py
+++ secao3/if_else.py
@@ -1,3 +1,5 @@
+'''
+
 velocidade = 100
 
 if velocidade > 100:
@@ -6,3 +8,15 @@ elif velocidade <= 100 and velocidade > 40:
     print("Velocidade permitida. Tente manter nesta velocidade.")
 else:
     print("Velocidade abaixo da permitida. Aumente a velocidade.")
+
+
+'''
+
+# exemplos do dia-a-dia
+valor_conta = 4000
+valor_emprestimo_a_pagar = 7500
+
+if valor_conta > valor_conta > valor_emprestimo_a_pagar:
+    print("Ufa! Sua parcela está em dia! Não faça mais nenhum empréstimo.")
+else:
+    print("Ixe! Está com bastante dívida! Pague as parcelas de seu empréstimo.")
