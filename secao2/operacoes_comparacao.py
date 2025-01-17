79cb0ae072c41b5e72d379f2c9fcd14d0d2ae681
bsohara <b.sohara.git@gmail.com>
2025-01-17 10:04:33 -0300

    operadores de comparacao e de atribuuicao

---
+++ secao2/operacoes_comparacao.py
@@ -0,0 +1,21 @@
+'''
+== -> igual
+!= -> diferente
+> -> maior que
+< -> menor que
+>= -> maior ou igual
+<= menor ou igual
+
+'''
+
+operadores = 10 == 10
+print(operadores)
+
+operadores2 = 10 != 1
+print(operadores2)
+
+operadores3 = 'Bruno' >= 'Bruno'
+print(operadores3)
+
+operadores3 = 'Bruno' != 'Bruno'
+print(operadores3)
