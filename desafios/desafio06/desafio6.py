39f56d89af92b2b6a8fa509e052d61b7d1dd0c74
bsohara <b.sohara.git@gmail.com>
2025-01-17 10:34:12 -0300

    desafios de listas

---
+++ desafios/desafio06/desafio6.py
@@ -0,0 +1,11 @@
+frutas = ['maçã', 'banana', 'manga', 'uva']
+
+for i in range(len(frutas)):
+    print(frutas[i])
+
+frutas_ordenadas = sorted(frutas)
+
+print(frutas_ordenadas)
+
+for i in range(len(frutas_ordenadas)):
+    print(frutas_ordenadas[i])
