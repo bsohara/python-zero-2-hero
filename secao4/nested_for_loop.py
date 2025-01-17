011752bc8a7d057a48457b9d513c683db18485b1
bsohara <b.sohara.git@gmail.com>
2025-01-17 12:22:09 -0300

    nested loop: exemplos de matriz kkkk

---
+++ secao4/nested_for_loop.py
@@ -0,0 +1,23 @@
+#loop aninhado
+
+'''
+
+- outer loop
+    - inner loop
+
+'''
+
+for a in range(1):
+    for b in range(1):
+        for c in range(1):
+            for d in range(1):
+                for e in range(1):
+                    for f in range(1):
+                        for g in range(1):
+                            for h in range(1):
+                                for i in range(1):
+                                    print(f'[{a}][{b}][{c}]')
+                                    print(f'[{d}][{e}][{f}]')
+                                    print(f'[{g}][{h}][{i}]')
+                                    print('\n')
+                            
