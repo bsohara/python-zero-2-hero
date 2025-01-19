08fc0e09490251b39eefc8383f953103f3eae05d
bsohara <b.sohara.git@gmail.com>
2025-01-19 09:35:28 -0300

    operações ternárias

---
+++ secao4/ternary_operator/ternary_operator.py
@@ -0,0 +1,14 @@
+idade = 16 
+
+#operação comum
+if idade >= 16:
+    resultado = print("Voto Obrigatório")
+else:
+    resultado = print("Voto Facultativo")
+
+print(resultado)
+
+# operação ternária
+resultado2 = 'Voto Permitido' if idade >= 16 else "Voto não permitido"
+
+print(resultado2)
