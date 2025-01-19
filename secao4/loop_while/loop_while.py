b4fd52dd7d3401b2ad2849afeab35cdc6069d3b5
bsohara <b.sohara.git@gmail.com>
2025-01-19 09:34:51 -0300

    descrição e diferenças entre loop e while

--- secao4/loop_while/loop_while.py
+++ secao4/loop_while/loop_while.py
@@ -1,9 +1,14 @@
 '''
-for...loop
+for...loop ==> quando sabe o que quer. ex: gire por 5 vezes
 ==========
+- aplicado em um alcance
+- como se fosse uma engrenagem ou roda
 
-while...loop
+
+while...loop ==> não sabe quantas vezes você quer que gire, a não ser por uma condição
 ============
+- aplicado uma condição
+- enquanto a máquina esteja ligada, gire algumas vezes
 
 '''
 
