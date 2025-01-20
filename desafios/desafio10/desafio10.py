e276c980294a75ab995e6f7bd0e38936d4db7f8d
bsohara <b.sohara.git@gmail.com>
2025-01-20 16:33:51 -0300

    desafios 9 e 10

---
+++ desafios/desafio10/desafio10.py
@@ -0,0 +1,7 @@
+'''
+for [variavel] in range([inicio], [fim*], [incremento])
+
+*obrigatório por requerir um ponto de parada. Sempre no -1
+'''
+for i in range(1, 11):
+    print(i)
---
+++ desafios/desafio11/desafio11.py
@@ -0,0 +1,7 @@
+carros = ['Mobi', 'Onix', 'HB20']
+motos = ['X96', 'Harley Davidson', 'CPX']
+
+for carro in carros:
+    for moto in motos:
+        print(f'[{carro}] == [{moto}]')
+    print()
