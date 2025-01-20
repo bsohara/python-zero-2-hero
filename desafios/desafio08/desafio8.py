5b1cce8ffef4add116eb388e40f71f1fc538200f
bsohara <b.sohara.git@gmail.com>
2025-01-20 16:12:25 -0300

    inserindo a solução do desafio 09

--- desafios/desafio08/desafio8.py
+++ desafios/desafio08/desafio8.py
@@ -1,3 +1,4 @@
+#desafio08
 frutas = ['Maçã', 'banana', 'manga', 'uva']
 print(frutas)
 
@@ -5,7 +6,19 @@ frutas[1] = 'morango' #adicionar no índice específico
 print(frutas)
 
 frutas.append('abacaxi') #adicionar no final da lista
+print(frutas)
 
 frutas[0:2] = ['goiaba', 'amora', 'abacate']
-
 print(frutas)
+
+frutas.insert(1, 'tomate')
+print(frutas)
+
+# desafio09
+frutas2 = ['maçã', 'morango', 'manga', 'uva', 'abacate']
+
+frutas2.remove('manga')
+print(frutas2)
+
+frutas2.pop()
+print(frutas2)
