b156d3154dd5f4a17a33d097251622756f4f55b0
bsohara <b.sohara.git@gmail.com>
2025-01-23 14:20:20 -0300

    inserindo exemplo em listas

---
+++ secao6/list_manipulation.py
@@ -0,0 +1,21 @@
+pessoas = ['Vanessa', 'Augusto', 'Bruno', 'Wesley', 'Caroline', 'Cecília', 'Rodrigo']
+
+# numeros = [2,3,4,5,6,7,8,9]
+
+# valores = [1.22, 2.33, 3.44, 4.55]
+
+print(pessoas[0])
+print(pessoas[-2])
+
+pessoas[0] = 'Pedro'
+pessoas[-1] = 'Letícia'
+
+print(pessoas)
+
+
+pessoas[-2] = 'Elenir'
+pessoas[2] = 'Eslier'
+pessoas[-3] = 'Guilherme'
+pessoas[-1] = 'Viviane'
+
+print(pessoas)
