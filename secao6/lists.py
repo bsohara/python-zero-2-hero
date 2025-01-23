b3bac4104e7e49ddce3043546bacc2126175caf3
bsohara <b.sohara.git@gmail.com>
2025-01-23 13:42:08 -0300

    listas

---
+++ secao6/lists.py
@@ -0,0 +1,22 @@
+'''
+
+# armazenar mais de um valor em uma única variável
+cidades = ['Rio de Janeiro', 'São Paulo', 'Salvador']
+
+# o mesmo que:
+cidade1 = 'Campinas'
+cidade2 = 'Sumaré'
+cidade3 = 'Piracicaba'
+
+print(cidades)
+
+for cidade in cidades:
+    print(cidade)
+
+'''
+
+# produtos
+produtos = ['sabonete', 'shampoo', 'condicionador', 'bucha', 'barbeador']
+
+for produto in produtos:
+    print(produto)
