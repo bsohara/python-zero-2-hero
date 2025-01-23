cffaf287b91b2239f28018d29aca15b1ac1be923
bsohara <b.sohara.git@gmail.com>
2025-01-23 10:43:35 -0300

    anotações sobre argumentos default e non-default

--- secao5/default_and_non_defaut.py
+++ secao5/default_and_non_defaut.py
@@ -1,4 +1,19 @@
-def my_function(my_list=None):
-    my_list = my_list or []
-    my_list.append(1)
-    print(my_list)
+'''
+Funções (functions)
+    - a. DRY ==> don't repeat yourself
+    - b. Parâmetros ==> argumentos
+    - c. Default ==> valor definido no parâmetro
+    - d. Non-default ==> valor não definido no parâmetro
+
+    ==> default e non-default devem estar em uma ordem
+
+    No exemplo abaixo, a variável 'nome' terá um nome de um produto. Então deverá ser passado.
+    Já na variável 'quantidade', como já foi definida no parâmetro, não será necessário passar na chamada da função
+'''
+
+def produto(nome, preco=10.99):
+    print(f"Produto: {nome}")
+    print(f"Preço: R$ {preco}")
+
+produto("chocolate")
+produto("chocolate", 9.99) #sobescrita
