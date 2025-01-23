fed0741270bd3ac4b9c05808183134b4e218ae06
bsohara <b.sohara.git@gmail.com>
2025-01-23 15:16:36 -0300

    list == extrações

---
+++ secao6/list_extraction.py
@@ -0,0 +1,11 @@
+produtos = ['arroz', 'feijão', 'banana', 'laranja']
+
+item1 = produtos[0]
+print(item1)
+
+for item in produtos:
+    print(item)
+
+item1, item2, item3, item4 = produtos
+
+print(item1, item2, item3, item4)
