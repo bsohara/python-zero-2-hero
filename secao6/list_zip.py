e8b40b05bf5749b144fb7bd556d5c4bde0e7db38
bsohara <b.sohara.git@gmail.com>
2025-01-25 14:03:04 -0300

    revisão de tudo

--- secao6/list_zip.py
+++ secao6/list_zip.py
@@ -1,3 +1,4 @@
+'''
 produtos = ['abacaxi', 'uva', 'banana', 'goiaba']
 valores = [1.99, 13.99, 10.99, 8.99]
 
@@ -9,3 +10,28 @@ print(var)
 
 duas_listas = zip(produtos, valores)
 print(list(duas_listas)) #==> uma tupla em uma lista
+
+'''
+numero_carros = int(input("Quantos carros estarão na lista? "))
+
+marcas = []
+modelos = []
+anos = []
+motores = []
+
+carros = zip(marcas, modelos, anos, motores)
+
+for i in range(numero_carros):
+    marca = input("Qual a marca do carro? ")
+    modelo = input("Qual modelo do carro? ")
+    ano = int(input("Qual ano deste carro? "))
+    motor = float(input("Qual o motor deste carro? "))
+
+    marcas.append(marca)
+    modelos.append(modelo)
+    anos.append(ano)
+    motores.append(motor) 
+
+print(list(carros))
+
+
