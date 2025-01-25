dd22e78fcca857b772b778709bee1f8aa0c410ad
bsohara <b.sohara.git@gmail.com>
2025-01-25 13:50:09 -0300

    adicionando zip e list com exemplos

---
+++ secao6/list_zip.py
@@ -0,0 +1,11 @@
+produtos = ['abacaxi', 'uva', 'banana', 'goiaba']
+valores = [1.99, 13.99, 10.99, 8.99]
+
+
+
+v = ['comprar'] #==> c o m p r a r
+var = list('comprar') #built-in ==> inserido no proprio python, sem necessidade de implantação
+print(var)
+
+duas_listas = zip(produtos, valores)
+print(list(duas_listas))
