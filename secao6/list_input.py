d73419266d178953f5ff784c277d3aa6d672f5f4
bsohara <b.sohara.git@gmail.com>
2025-01-31 17:39:48 -0300

    input de listas - split

---
+++ secao6/list_input.py
@@ -0,0 +1,9 @@
+# captura de informações para inserir uma lista
+
+#1. split
+
+carros_usuario = input('Qual o nome dos carros separados por vírgula?')
+
+carros_lista = carros_usuario.split(' - ')
+
+print(carros_lista)
