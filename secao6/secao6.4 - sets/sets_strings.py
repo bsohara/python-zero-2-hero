53f4b715016e37264c54e85842616d404bfd0749
bsohara <b.sohara.git@gmail.com>
2025-02-06 19:03:55 -0300

    métodos de sets com strings

--- secao6/secao6.4 - sets/sets_strings.py
+++ secao6/secao6.4 - sets/sets_strings.py
@@ -2,4 +2,25 @@
 
 sets com strings
 
+- operadores de sets com strings
+
+
 '''
+
+
+set1 = {'a', 'b', 'c'}
+set2 = {'a', 'b', 'd', 'e'}
+set3 = {'a', 'c', 'f', 'g'}
+
+set4 = set1.union(set2)                  # union ==> união de sets sem que apareça diferentes
+set5 = set1.difference(set3)             # difference ==> diferença de valores
+set6 = set1.intersection(set2)           # intersection ==> o mesmo valor aparece nas duas listas
+set7 = set1.symmetric_difference(set3)   # symetric_difference ==> retira os valores duplicados
+
+print(set4)
+
+print(set5)
+
+print(set6)
+
+print(set7)
