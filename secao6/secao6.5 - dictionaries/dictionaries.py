0719235a453f64ca402b9289c678c781d32161da
bsohara <b.sohara.git@gmail.com>
2025-02-06 19:15:10 -0300

    dicionarios

---
+++ secao6/secao6.5 - dictionaries/dictionaries.py
@@ -0,0 +1,15 @@
+'''
+Dicionários
+- utiliza index no formato de key (chave) e value (valor)
+- ex: aluno: { 'nome' (key):  'Bruno' (value)}
+'''
+
+aluno = {
+    'nome': 'Bruno',
+    'idade': 28,
+    'RA': '2223970',
+    'notas': [9.5, 9.0, 8.5, 9.0]
+}
+
+print(aluno)
+print(aluno['notas'])
