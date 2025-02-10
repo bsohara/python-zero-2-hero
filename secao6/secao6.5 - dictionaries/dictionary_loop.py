98b08fed9b135d3b57eee185bd4462913f1727a3
bsohara <b.sohara.git@gmail.com>
2025-02-10 08:22:16 -0300

    correção de espaçamentos

--- secao6/secao6.5 - dictionaries/dictionary_loop.py
+++ secao6/secao6.5 - dictionaries/dictionary_loop.py
@@ -5,14 +5,18 @@ aluno = {
     'aprovado': True
 }
 
+print()
 for a in aluno.keys():
     print(a)
 
+print()
 for a in aluno.values():
     print(a)
 
+print()
 for a in aluno.items():
     print(a)
 
+print()
 for keys, values in aluno.items():
-    print(keys, values)
+    print(keys,"=", values)
