53f2561d4ac2f078d080dc0be5553f351b0e095a
bsohara <b.sohara.git@gmail.com>
2025-02-10 08:31:21 -0300

    dicionários - chaves, valores e itens

---
+++ secao6/secao6.5 - dictionaries/dictionary_key_value.py
@@ -0,0 +1,18 @@
+aluno = {
+    'nome': 'Bruno',
+    'idade': 16,
+    'nota_final': 'A',
+    'aprovado': True,
+    'disciplinas': ['Física', 'Matemática', 'Inglês']
+}
+
+print()
+print(aluno.get('disciplinas'))
+print()
+print(len(aluno))
+print()
+print(aluno.items())
+print()
+print(aluno.keys())
+print()
+print(aluno.values())
